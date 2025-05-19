e891d3c32d31fd731050e8a46082c6eb7ba1a454
Arjob Mukherjee <arjobmukherjee@gmail.com>
2025-05-19 23:23:24 +0530

    Multiple changes to get input & display status

--- c/main.c
+++ c/main.c
@@ -1,47 +1,100 @@
 #include <avr/io.h>
 #include <util/delay_basic.h>
+#include <avr/interrupt.h>
+#include <stdbool.h>
+#include "main.h"
 
-#define BAUD 10000
-#define BAUD_COUNTER (((unsigned int)F_CPU/(16 * BAUD)) - 1)
+#define SWITCH_PIN_NO PD0
+#define SWITCH_PORT   PORTD
+#define SWITCH_PIN    PIND
+#define SWITCH_DDR    DDRD
 
-#define HIGH8(v) ((v >> 8) & 0xFF)
-#define LOW8(v) (v & 0xFF)
+#define STATUS_PIN_NO PB1
+#define STATUS_PORT   PORTB
+#define STATUS_PIN    PINB
+#define STATUS_DDR    DDRB
 
-void usart_setup();
-void usart_send_char(char c);
-void usart_send_string(const char* str);
+typedef enum TestModes {
+    UNKNOWN_TEST,
+    USART_TEST,
+    HOLDOFF_TEST,
+    TEST_COUNT
+} TestModes;
 
-__attribute__((noreturn))
-void main(void) {
-    usart_setup();
+static TestModes mode;
+static bool mode_isDirty;
 
-    for(char c = 'A'; ; c+1 > 'Z' ? c = 'A' : c++) {
-        _delay_loop_2(65535);
-        usart_send_string("Arjob");
-        usart_send_char(c);
+static void usart_test();
+static void holdoff_test();
+
+ISR (INT0_vect)
+{
+    if (++mode == TEST_COUNT) {
+        mode = 1;
     }
-    while(1);
+
+    BIT_SET(STATUS_PORT, STATUS_PIN_NO);
+
+    // Wait for the pin to be high
+    while (!IS_BIT_SET (SWITCH_PIN, SWITCH_PIN_NO))
+        ;
+
+    BIT_CLEAR(STATUS_PORT, STATUS_PIN_NO);
 }
 
-void usart_setup()
+void setup_io()
 {
-    UBRRH = 0; //HIGH8(BAUD_COUNTER);
-    UBRRL = 0x4A; //LOW8(BAUD_COUNTER);
+    // Make the Interrupt 0 pin as the input
+    MAKE_PIN_INPUT_PULLUP (SWITCH_DDR, SWITCH_PORT, SWITCH_PIN_NO);
 
-    UCSRC = (1 << URSEL) | (1 << UCSZ1) | (1 << UCSZ0) | (1 << USBS);
-    UCSRB = (1 << TXEN);
+    // Enable interrupt
+    BIT_CLEAR_MASK (MCUCR, (1 << ISC01 | 1 << ISC00)); // Trigger at Low level
+    BIT_SET_MASK (GICR, (1 << INT0));                  // Enable the interrupt
+
+    // Status port
+    MAKE_PIN_OUTPUT (STATUS_DDR, STATUS_PIN_NO);
+    BIT_SET (STATUS_PORT, STATUS_PIN_NO);
 }
 
-void usart_send_string(const char* str)
+__attribute__ ((noreturn)) void main (void)
 {
-    char c;
-    while ((c = *str++)) {
-        usart_send_char(c);
+    mode         = USART_TEST;
+    mode_isDirty = true;
+
+    setup_io();
+
+    while (1) {
+        mode_isDirty = false;
+        switch (mode) {
+        case USART_TEST:
+            usart_test();
+            break;
+        case HOLDOFF_TEST:
+            holdoff_test();
+            break;
+        default:
+            BIT_SET (STATUS_PORT, STATUS_PIN_NO);
+            break;
+        }
     }
 }
 
-void usart_send_char(char c)
+void holdoff_test()
 {
-    while ((UCSRA & (1 << UDRE)) == 0);
-    UDR = c;
+#define OUTPUT_PIN_NO PD1
+#define OUTPUT_PORT   PORTD
+#define OUTPUT_PIN    PIND
+#define OUTPUT_DDR    DDRD
+
+}
+
+void usart_test()
+{
+    usart_on();
+    for (char c = 'A'; !mode_isDirty; c + 1 > 'Z' ? c = 'A' : c++) {
+        _delay_loop_2 (65535);
+        usart_send_string ("Arjob");
+        usart_send_char (c);
+    }
+    usart_off();
 }
